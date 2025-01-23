b6b289eee071472eaa5eeb5a3dc6ce099b9841fc
BugHunter-X <askzenai@gmail.com>
2025-01-23 18:10:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,27 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error if the about.js file is empty
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+}
+```
